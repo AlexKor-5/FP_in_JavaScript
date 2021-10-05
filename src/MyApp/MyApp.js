57aac28dd1530324a4c3db3f193f376b4deb450c
Alexander Korniichuk <world.kor.films@gmail.com>
2021-10-05 11:40:37 +0300

    FP using reduce and map

--- src/MyApp/MyApp.js
+++ src/MyApp/MyApp.js
@@ -15,22 +15,37 @@ export const MyApp = () => {
     // const pluck = R.pluck
 
     const persons = [
-        {id: 345, fullName: "Alexander Kor"},
-        {id: 890, fullName: "Alice Merton"},
-        {id: 234, fullName: "John Abram"},
-        {id: 564, fullName: "Memphice Lee"},
-        {}
+        {id: 345, fullName: "Alexander Kor", address: {email: "", country: "US", city: "Oland"}},
+        {id: 890, fullName: "Alice Merton", address: {email: "", country: "UK", city: "London"}},
+        {id: 234, fullName: "John Abram", address: {email: "", country: "UK", city: "London"}},
+        {id: 564, fullName: "Memphice Lee", address: {email: "", country: "UKR", city: "Lviv"}}
     ]
 
-    // console.log(persons)
-    // const res = _.map(persons, (item) => item.fullName !== null && item.fullName !== undefined ? item.fullName : '')
-    // console.log(res)
+    const result = _(persons).reduce((stat, person) => {
+        const country = person.address.country;
+        stat[country] = _.isUndefined(stat[country]) ? 1 : stat[country] + 1;
+        return stat;
+    }, {});
 
-    const data = [1, 2, 4]
-    console.log(data)
-    _(data).reverse().map(item => console.log(item)).value()
+    console.log("result = ", result)
+    ///////////////////////////////////////////////
 
+    const getCountry = person => person.address.country;
 
+    const gatherStats = function (stat, criteria) {
+        stat[criteria] = _.isUndefined(stat[criteria]) ? 1 : stat[criteria] + 1;
+        return stat;
+    };
+
+    const my_result = _(persons).map(getCountry).reduce(gatherStats, {});
+    console.log("my_result = ", my_result)
+    /////////////////////////////////////////////////
+
+    const cityPath = ['address', 'city'];
+    const countryLens = R.lens(R.path(cityPath), R.assocPath(cityPath));
+
+    console.log(_(persons).map(R.view(countryLens)).reduce(gatherStats, {}));
+    console.log(_.groupBy(persons, R.view(countryLens)))
 
     return (
         <>
