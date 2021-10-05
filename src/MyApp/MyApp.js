c004ead62596e9990d92767ab25bc0e806fe5a7f
Alexander Korniichuk <world.kor.films@gmail.com>
2021-10-05 09:45:35 +0300

    working _() operator (MyApp.js) ex

--- src/MyApp/MyApp.js
+++ src/MyApp/MyApp.js
@@ -1,32 +1,36 @@
 import React from "react"
 import * as R from 'ramda'
-import db from "./fakeDB"
+// import db from "./fakeDB"
+import * as _ from 'lodash'
 
 export const MyApp = () => {
     // const taker = document.getElementById('taker')
-    const curry = R.curry
-    const run = R.compose
-    const sum = R.sum
-    const divide = R.divide
-    const input = [30, 50, 10, 79]
-    const subtract = R.subtract
-    const multiply = R.multiply
-    const pluck = R.pluck
+    // const curry = R.curry
+    // const run = R.compose
+    // const sum = R.sum
+    // const divide = R.divide
+    // const input = [30, 50, 10, 79]
+    // const subtract = R.subtract
+    // const multiply = R.multiply
+    // const pluck = R.pluck
 
-    const size = arr => arr.length
-    const average = arr => divide(sum(arr), size(arr))
-    // console.log("average = ", average(input))
-    const data = [
-        {name: "Alex", surname: "Kor", id: 450},
-        {name: "Mike", surname: "Jophrich", id: 450},
-        {name: "Alice", surname: "Merton", id: 450},
-        {name: "John", surname: "Lue", id: 450}
+    const persons = [
+        {id: 345, fullName: "Alexander Kor"},
+        {id: 890, fullName: "Alice Merton"},
+        {id: 234, fullName: "John Abram"},
+        {id: 564, fullName: "Memphice Lee"},
+        {}
     ]
+
+    // console.log(persons)
+    // const res = _.map(persons, (item) => item.fullName !== null && item.fullName !== undefined ? item.fullName : '')
+    // console.log(res)
+
+    const data = [1, 2, 4]
     console.log(data)
-    // console.log(data.pluck("name"))
-    const pluckNames = pluck("surname")
-    // console.log(pluckNames);
-    console.log(pluckNames(data))
+    _(data).reverse().map(item => console.log(item)).value()
+
+
 
     return (
         <>
