f0d922470f32632a02c7824fdd0c4e484b941499
Alexander Korniichuk <world.kor.films@gmail.com>
2021-12-06 09:48:30 +0200

    The type Wrapper Monad

--- src/MyApp/MyApp.js
+++ src/MyApp/MyApp.js
@@ -17,80 +17,89 @@ export const MyApp = () => {
     const isValid = val => !_.isUndefined(val) && !_.isNull(val);
 
 
+    // class Wrapper {
+    //     constructor(value) {
+    //         this._value = value;
+    //     }
+    //
+    //     map(f) {
+    //         return f(this._value);
+    //     }
+    //
+    //
+    //     fmap(f) {
+    //         return new Wrapper(f(this._value));
+    //     }
+    //
+    //     toString() {
+    //         return 'Wrapper (' + this._value + ')';
+    //     }
+    // }
+    //
+    // const wrap = (val) => new Wrapper(val)
+    //
+    // const wrappedValue = wrap('Get Functional');
+    // console.log(wrappedValue.map(R.identity));
+    //
+    // const plus = R.curry((a, b) => a + b);
+    // const plus3 = plus(3);
+    // const plus10 = plus(10);
+    //
+    // const two = wrap(2);
+    //
+    //
+    // const value = two.fmap(plus3).fmap(plus10)
+    // console.log(value.map(R.identity)); // -> 15
+
+/////////////////////////// monad /////////////////////////////////////////////////////////
+
+
     class Wrapper {
         constructor(value) {
             this._value = value;
         }
 
-        map(f) {
-            return f(this._value);
+        static of(a) {
+            return new Wrapper(a);
         }
 
+        map(f) {
+            return Wrapper.of(f(this._value));
+        }
 
-        fmap(f) {
-            return new Wrapper(f(this._value));
+        join() {
+            if (!(this._value instanceof Wrapper)) {
+                return this;
+            }
+            return this._value.join();
+        }
+
+        get() {
+            return this._value
         }
 
         toString() {
-            return 'Wrapper (' + this._value + ')';
+            return `Wrapper (${this._value})`;
         }
     }
 
-    const wrap = (val) => new Wrapper(val)
+    const res = Wrapper.of('Hello Monads!')
+        .map(R.toUpper)
+        .map(R.identity); //-> Wrapper('HELLO MONADS!')
+    console.log(res)
+//////////////////////
 
-    const wrappedValue = wrap('Get Functional');
-    console.log(wrappedValue.map(R.identity));
-
-    const plus = R.curry((a, b) => a + b);
-    const plus3 = plus(3);
-    const plus10 = plus(10);
-
-    const two = wrap(2);
-
-
-    const value = two.fmap(plus3).fmap(plus10)
-    // console.log(value.map(R.identity)); // -> 15
-
-///////////////////////////////////////////////////////////////////////////////////////
-    const findStudent = R.curry(function (db, ssn) {
-        return wrap(db.get(ssn));
+    const findObject = R.curry((db, id) => {
+        return Wrapper.of(db.get(id));
     });
-    const getAddress = function (student) {
-        return wrap(student.fmap(R.prop('address')));
-    }
 
-    const studentAddress = R.compose(
-        getAddress,
-        findStudent(db)
-    );
-    console.log(studentAddress('444-44-4444').map(R.identity).map(R.identity));
+    const getAddress = student => {
+        return Wrapper.of(student.map(R.prop('address')));
+    };
 
-    const imp_studentAddress = ssn => {
-        const st = db.get(ssn)
-        return st.address
-    }
+    const studentAddress = R.compose(getAddress, findObject(db));
 
-    console.log("imp_ = ", imp_studentAddress('444-44-4444'));
-
-///////////////////////////////////////////////////////////////////////////////////////
-
-    class Empty {
-        map(f) {
-            return this
-        }
-
-        toString() {
-            return 'Empty ()';
-        }
-    }
-
-    const empty = () => new Empty()
-
-
-    const isEven = (n) => Number.isFinite(n) && (n % 2 === 0);
-    const half = (val) => isEven(val) ? wrap(val / 2) : empty();
-    console.log(half(4));  // -> ok
-    console.log(half(3));  // -> not
+    console.log(studentAddress('444-44-4444').join().get());
 
 
     return (
