70b2032e38b71da3917637fee1e42b11f5162f79
Alexander Korniichuk <world.kor.films@gmail.com>
2021-12-06 10:38:19 +0200

    Maybe Monad ex of using

--- src/MyApp/MyApp.js
+++ src/MyApp/MyApp.js
@@ -86,7 +86,7 @@ export const MyApp = () => {
     const res = Wrapper.of('Hello Monads!')
         .map(R.toUpper)
         .map(R.identity); //-> Wrapper('HELLO MONADS!')
-    console.log(res)
+    // console.log(res)
 //////////////////////
 
     const findObject = R.curry((db, id) => {
@@ -99,7 +99,101 @@ export const MyApp = () => {
 
     const studentAddress = R.compose(getAddress, findObject(db));
 
-    console.log(studentAddress('444-44-4444').join().get());
+    // console.log(studentAddress('444-44-4444').join().get());
+
+//////////////////////////////////////////////////
+
+    class Maybe {
+        static just(a) {
+            return new Just(a);
+        }
+
+        static nothing() {
+            return new Nothing();
+        }
+
+        static fromNullable(a) {
+            return a !== null ? Maybe.just(a) : Maybe.nothing();
+        }
+
+        static of(a) {
+            return Maybe.just(a);
+        }
+
+        get isNothing() {
+            return false;
+        }
+
+        get isJust() {
+            return false;
+        }
+    }
+
+    class Just extends Maybe {
+        constructor(value) {
+            super();
+            this._value = value;
+        }
+
+        get value() {
+            return this._value;
+        }
+
+        map(f) {
+            return Maybe.of(f(this._value));
+        }
+
+        getOrElse() {
+            return this._value;
+        }
+
+        filter(f) {
+            Maybe.fromNullable(f(this._value) ? this._value : null);
+        }
+
+        chain(f) {
+            return f(this._value)
+        }
+
+        toString() {
+            return `Maybe.Just(${this._value})`;
+        }
+    }
+
+    class Nothing extends Maybe {
+        map(f) {
+            return this;
+        }
+
+        get value() {
+            throw new TypeError("Can't extract the value of a Nothing.");
+        }
+
+        getOrElse(other) {
+            return other;
+        }
+
+        filter() {
+            return this._value;
+        }
+
+        chain(f) {
+            return f(this._value)
+        }
+
+        toString() {
+            return 'Maybe.Nothing';
+        }
+    }
+
+    const safeFindObject = R.curry((db, id) => {
+        return Maybe.fromNullable(db.get(id));
+    });
+
+    const safeFindStudent = safeFindObject(db);
+
+    const address = safeFindStudent('444-44-444').map(R.prop('address'));
+    console.log("address = ", address.getOrElse("Whoops"));
 
 
     return (
